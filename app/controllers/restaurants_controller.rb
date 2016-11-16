d7d8f1c09dd72b4358cf0ae76bc08eec38bf91ff
Murugan <murugu2012@gmail.com>
2016-11-16 14:09:20 +0530

    Integrate Ransack

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,6 +1,7 @@
 class RestaurantsController < ApplicationController
   def index
-    @restaurants = Restaurant.page(params[:page]).per(10)
+    @q = Restaurant.ransack(params[:q])
+    @restaurants = @q.result(:distinct => true).includes(:ownerships, :reviews, :taggings, :owners, :reviewers, :tags).page(params[:page]).per(10)
 
     render("restaurants/index.html.erb")
   end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,7 @@
 class ReviewsController < ApplicationController
   def index
-    @reviews = Review.page(params[:page]).per(10)
+    @q = Review.ransack(params[:q])
+    @reviews = @q.result(:distinct => true).includes(:restaurant, :user, :pictures, :responses).page(params[:page]).per(10)
 
     render("reviews/index.html.erb")
   end
