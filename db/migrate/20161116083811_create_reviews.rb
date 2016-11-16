9f3c6abc2dd881212f062bf14fa182c5e3b41af0
Murugan <murugu2012@gmail.com>
2016-11-16 14:08:13 +0530

    Generate reviews resource with starter:resource

---
+++ db/migrate/20161116083811_create_reviews.rb
@@ -0,0 +1,13 @@
+class CreateReviews < ActiveRecord::Migration
+  def change
+    create_table :reviews do |t|
+      t.integer :user_id
+      t.integer :restaurant_id
+      t.text :content
+      t.integer :stars
+
+      t.timestamps
+
+    end
+  end
+end
